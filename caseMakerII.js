4e0fac75417dded2c0bf736ac2f2ab648065c1b5
noahthomlison <noahthomlison@gmail.com>
2021-04-15 03:02:49 +0000

    updated kata for square code

--- caseMakerII.js
+++ caseMakerII.js
@@ -1,86 +1,71 @@
 const makeCase = function(input, type) {
   // Put your solution here
-
   let newString = '';
-  let makeUpper = '';
-  let makeFirstUpper = '';
-  let noSpace = '';
-  let checkChar = '';
-
-  switch(type) {
-case 'camel':
-  makeFirstUpper = input.replace( / (.)/g, function($1) { return $1.toUpperCase(); });
-  noSpace = makeFirstUpper.replace(/ /g, '');
-  return(noSpace)
-  break;
-
-case 'pascal':
-  makeFirstUpper = input.replace( / (.)/g, function($1) { return $1.toUpperCase(); });
-  noSpace = makeFirstUpper.replace(/ /g, '');
+  let index = 0;
+  let spaceLocation = input.indexOf(' ');
+  let i = 0;
 
 
-  return(newString2)
-  break;
-
-case 'snake':
-  newString = input.replace(/ /g, '_');
-  return(newString);
-  break;
-
-case 'kebab':
-  newString = input.replace(/ /g, '-');
-  return(newString);
-  break;
-
-case 'title':
-  break;
-
-case 'vowel':
-  break;
-
-case 'consonant':
-  newString = input.replace(/ /g, '-');
-  makeUpper = newString.toUpperCase();
-  return (makeUpper);
-  break;
-
-  /*
-    // Replaces any - or _ characters with a space 
-    .replace( /[-_]+/g, ' ')
-    // Removes any non alphanumeric characters 
-    .replace( /[^\w\s]/g, '')
-    // Uppercases the first character in each group immediately following a space 
-    // (delimited by spaces) 
-    .replace( / (.)/g, function($1) { return $1.toUpperCase(); })
-    // Removes spaces 
-    .replace( / /g, '' );
-*/
-
-
-  /*
-  for (i = 0; i < input.length; i++){
-    checkChar = input.charAt(i);
-
-    if(checkChar == ' '){
-      makeUpper = input[i+1].toUpperCase()
-      i = i + 1;
-      newString = newString + makeUpper;
+  for(i = 0; i < input.length; i++){
+    if(input[i]===' '){
+      newString[i+1] = input[i+1].toUpperCase() 
+      console.log(newString)
     }
     else{
-    newString = newString + input[i].charAt();
-    } 
+      newString[i] = input[i]
+      console.log(newString)
+    }
   }
-  */
+  console.log(newString)
+
+
+  // newString[i] = input[input.indexOf(' ')+1].toUpperCase()
+
+  // for(i = 0; i < input.length; i++){
+  //   if(i === input[i].indexOf(''))
+  //   newString[i] = input[input.indexOf(' ')+1].toUpperCase()
+
+  // }
+  
+  // console.log(test)
+  // while(input.indexOf(' ') > -1){
+  //   test = input.toUpperCase(input.indexOf(' ')+1)
+  // }
+
+  // // for(let i = 0; i < input.length; i++){
+  // //   if(input[i] === ' '){
+  //     spaceLocation[i] = 1
+  //   }
+  //   else{
+  //     spaceLocation[i] = 0
+  //   }
+  // }
+  // if(type === 'camel'){
+  //   i = 0;
+  //   // messageFiltered = input.replace(/ /g,"")
+  //   while(newString.length < input.length){
+
+  //     if(spaceLocation[i] === 1){
+  //       newString = newString + input[i+1].toUpperCase()
+  //       console.log('ding')
+  //     }
+  //     else{
+  //       newString = newString + input[i]
+  //     } 
+  //     i++
+   
+  //   }
+  // }  
   return(newString);  
 }
-}
+
 
 console.log(makeCase("this is a string", "camel"));
-console.log(makeCase("this is a string", "pascal"));
-console.log(makeCase("this is a string", "snake"));
-console.log(makeCase("this is a string", "kebab"));
-console.log(makeCase("this is a string", "title"));
+// console.log(makeCase("this is a string", "pascal"));
+// console.log(makeCase("this is a string", "snake"));
+// console.log(makeCase("this is a string", "kebab"));
+// console.log(makeCase("this is a string", "title"));
 
-console.log(makeCase("this is a string", "vowel"));
-console.log(makeCase("this is a string", "consonant"));
-console.log(makeCase("this is a string", ["upper", "snake"]));
+// console.log(makeCase("this is a string", "vowel"));
+// console.log(makeCase("this is a string", "consonant"));
+// console.log(makeCase("this is a string", ["upper", "snake"]));
