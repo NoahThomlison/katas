9c5303312ac4d2fcf83f44a63b9ea5457c103ee5
noahthomlison <noahthomlison@gmail.com>
2021-07-14 03:56:46 +0000

    starting this kata again

--- caseMakerII.js
+++ caseMakerII.js
@@ -1,71 +1,62 @@
-const makeCase = function(input, type) {
-  // Put your solution here
-  let newString = '';
-  let index = 0;
-  let spaceLocation = input.indexOf(' ');
-  let i = 0;
+const makeCase = function(input, type)  {
+  splitString = input.split(' ')
 
-
-  for(i = 0; i < input.length; i++){
-    if(input[i]===' '){
-      newString[i+1] = input[i+1].toUpperCase() 
-      console.log(newString)
-    }
-    else{
-      newString[i] = input[i]
-      console.log(newString)
-    }
-  }
-  console.log(newString)
-
-
-  // newString[i] = input[input.indexOf(' ')+1].toUpperCase()
-
-  // for(i = 0; i < input.length; i++){
-  //   if(i === input[i].indexOf(''))
-  //   newString[i] = input[input.indexOf(' ')+1].toUpperCase()
-
-  // }
-  
-  // console.log(test)
-  // while(input.indexOf(' ') > -1){
-  //   test = input.toUpperCase(input.indexOf(' ')+1)
-  // }
-
-  // // for(let i = 0; i < input.length; i++){
-  // //   if(input[i] === ' '){
-  //     spaceLocation[i] = 1
-  //   }
-  //   else{
-  //     spaceLocation[i] = 0
-  //   }
-  // }
-  // if(type === 'camel'){
-  //   i = 0;
-  //   // messageFiltered = input.replace(/ /g,"")
-  //   while(newString.length < input.length){
-
-  //     if(spaceLocation[i] === 1){
-  //       newString = newString + input[i+1].toUpperCase()
-  //       console.log('ding')
-  //     }
-  //     else{
-  //       newString = newString + input[i]
-  //     } 
-  //     i++
-   
-  //   }
-  // }  
-  return(newString);  
+  console.log(capitalizeFirstLetter(splitString[0]))
 }
 
+function capitalizeFirstLetter(input){
+  let newString = ''
 
-console.log(makeCase("this is a string", "camel"));
+  newString += input[0].toUpperCase()
+  for (let i = 1; i < input.length; i++){
+    newString += input[i]
+  }
+  
+  return(newString)
+}
+
+makeCase("this is a string", 2);
 // console.log(makeCase("this is a string", "pascal"));
 // console.log(makeCase("this is a string", "snake"));
 // console.log(makeCase("this is a string", "kebab"));
 // console.log(makeCase("this is a string", "title"));
-
 // console.log(makeCase("this is a string", "vowel"));
 // console.log(makeCase("this is a string", "consonant"));
 // console.log(makeCase("this is a string", ["upper", "snake"]));
+
+
+// thisIsAString remove all spaces, capitalize first letter after space
+// ThisIsAString remove all spaces, capitalize firts letter in all words
+// this_is_a_string 
+// this-is-a-string
+// This Is A String
+// thIs Is A strIng
+// THiS iS a STRiNG 
+// THIS_IS_A_STRING
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
